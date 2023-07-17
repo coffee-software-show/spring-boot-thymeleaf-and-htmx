533790cee7b3bb2d4458e78bcbcd7eadce786fb4
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-07-17 15:15:12 +0200

    use functional endpoints (MVC.fn)
    Since HTMXResponse can't be returned with ServerResponse.ok(), find other solution for updating the form with Add-button - namely by setting the hx-target to body (swapping all body content).

--- src/main/java/com/example/htmx/HtmxApplication.java
+++ src/main/java/com/example/htmx/HtmxApplication.java
@@ -1,23 +1,27 @@
 package com.example.htmx;
 
-import io.github.wimdeblauwe.hsbt.mvc.HtmxResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
 
 import java.time.Instant;
-import java.util.stream.Stream;
+import java.util.Arrays;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootApplication
 public class HtmxApplication {
@@ -26,6 +30,16 @@ public class HtmxApplication {
         SpringApplication.run(HtmxApplication.class, args);
     }
 
+    @Bean
+    RouterFunction<ServerResponse> htmlRoutes(TodosHandler handler) {
+        return route()
+                .nest(RequestPredicates.path("/todos"), builder -> builder
+                        .GET(handler::todos)
+                        .POST( "/reset", handler::reset)
+                        .POST( handler::add)
+                        .DELETE("/{todoId}", handler::delete))
+                .build();
+    }
 }
 
 @Component
@@ -38,10 +52,11 @@ class Initializer {
     }
 
     @EventListener({ApplicationReadyEvent.class, TodosResetEvent.class})
-    void reset() {
+    public void reset() {
         this.repository.deleteAll();
-        Stream.of("write a new blog,record a video on HTMX,record a new podcast episode".split(","))
-                .forEach(t -> this.repository.save(new Todo(null, t)));
+        Arrays.stream("write a new blog,record a video on HTMX,record a new podcast episode".split(","))
+                .map(s -> new Todo(null, s))
+                .forEach(repository::save);
     }
 }
 
@@ -52,48 +67,39 @@ class TodosResetEvent extends ApplicationEvent {
 }
 
 @Slf4j
-@Controller
-@RequestMapping("/todos")
-class TodosController {
+@Service
+class TodosHandler {
 
     private final TodoRepository repository;
 
     private final ApplicationEventPublisher publisher;
 
-    TodosController(TodoRepository repository,
+    TodosHandler(TodoRepository repository,
                     ApplicationEventPublisher publisher) {
         this.repository = repository;
         this.publisher = publisher;
     }
 
-    @PostMapping("/reset")
-    String reset(Model model) {
-        this.publisher.publishEvent(new TodosResetEvent());
-        model.addAttribute("todos", this.repository.findAll());
-        return "todos :: todos";
+    ServerResponse reset(ServerRequest request) {
+        publisher.publishEvent(new TodosResetEvent());
+        return ok().render("todos :: todos", this.repository.findAll());
     }
 
-    @PostMapping
-    HtmxResponse add(@RequestParam("new-todo") String newTodo, Model model) {
+    ServerResponse add(ServerRequest request) {
+        var newTodo = request.param("new-todo").orElseThrow();
         log.debug("going to add another todo : " + newTodo);
-        this.repository.save(new Todo(null, newTodo));
-        model.addAttribute("todos", this.repository.findAll());
-        return new HtmxResponse()
-                .addTemplate("todos :: todos")
-                .addTemplate("todos :: todos-form");
+        repository.save(new Todo(null, newTodo));
+        return ok().render("todos", repository.findAll());
     }
 
-    @ResponseBody
-    @DeleteMapping(path = "/{todoId}", produces = MediaType.TEXT_HTML_VALUE)
-    String delete(@PathVariable Integer todoId) {
-        this.repository.findById(todoId).ifPresent(this.repository::delete);
-        return "";
+    ServerResponse delete(ServerRequest request) {
+        var todoId = Integer.parseInt(request.pathVariable("todoId"));
+        repository.deleteById(todoId);
+        return ok().build();
     }
 
-    @GetMapping
-    String todos(Model model) {
-        model.addAttribute("todos", this.repository.findAll());
-        return "todos";
+    ServerResponse todos(ServerRequest ignoredRequest) {
+        return ok().render("todos", repository.findAll());
     }
 }
 
